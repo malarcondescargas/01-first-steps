b12f99ec0760f61d6381b32ac547f4ab68fb2580
malarcondescargas <malarcon.descargas@gmail.com>
2026-04-27 20:23:04 -0400

    Nextjs 01

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,11 @@
+
+export const metadata = {
+    title: 'About',
+    description: 'SEO Title',
+    keywords: ['About', 'Marco']
+};
+export default function AboutPage() {
+    return (
+        <span className="text-lg">About</span>
+    )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'PRICING',
+    description: 'Princing page',
+};
+
+export default function PricingPage() {
+    return (
+        <>
+            <span className="text-7xl">Pricing Page</span>
+        </>
+    )
+}
